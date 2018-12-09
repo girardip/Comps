304e03f8ab891aaf6da8eb8cf0d2c15ea010a7e6
Pedro Girardi <pedrogirardi16@gmail.com>
2018-12-09 11:40:15 +0000

    update

--- mergingAndRelabeling.R
+++ mergingAndRelabeling.R
@@ -22,3 +22,18 @@ relabeling_colnames = function(pnad_pesquisa, pnad_dic){
 # V4750
 # 12
 # REND. MENSAL FAM. PER CAPITA
+
+download_sourceData("PNAD", 2012, unzip = T)
+install.packages("devtools")
+install.packages("stringi") 
+devtools::install_github("lucasmation/microdadosBrasil")
+library('microdadosBrasil')
+d2 <- read_PNAD("pessoas", 2012, file = "PNAD_reponderado_2012_20150814/2012/Dados/PES2012.TXT")
+saveRDS(d2, "Data/PNAD/PNAD2012.rds")
+# https://downloads.ibge.gov.br/downloads_estatisticas.htm?caminho=Trabalho_e_Rendimento/Pesquisa_Nacional_por_Amostra_de_Domicilios_continua/Trimestral/Microdados/
+
+
+
+# try 2 
+
+pnad112 = read_PNADcontinua("pessoas", i = "2012-1q", file = "Data/PNAD_CONT/PNADC_012012_20180816.txt")
