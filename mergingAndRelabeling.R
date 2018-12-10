e50d03ad72f93164eaa27eefdb8a1e23838191b6
Pedro Girardi <pedrogirardi16@gmail.com>
2018-12-10 19:39:13 +0000

    changes in merging and relabeling

--- mergingAndRelabeling.R
+++ mergingAndRelabeling.R
@@ -1,4 +1,6 @@
 library(tidyverse)
+library(data.table)
+
 setwd("PME/")
 # does not run
 df <- list.files(pattern = ".rds") %>%
@@ -7,3 +9,30 @@ df <- list.files(pattern = ".rds") %>%
 # try own for loop
 reduced_list = list.files(pattern = ".rds")[1:10]
 reduced_df = map_dfr(reduced_list, readRDS)
+fwrite(reduced_df, "~/Comps/reduced_df1.csv")
+
+csvWriter = function(filesList){
+  for (file in filesList){
+    partial_df = map_dfr(reduced_list, readRDS)
+    write.csv(partial_df, file = paste0(str_sub(file, end = -4),"csv"))
+  }
+}
+
+
+# reading and merging
+
+listFiles =list.files("PME_csv/")
+setwd("PME_csv/")
+ab = F
+for (file in listFiles){
+  if (ab==F) {
+    completePME = read_csv(file)
+    ab=T
+  }
+  if (ab==T) {
+    completePME = completePME %>%
+      bind_rows(read_csv(file))
+  }
+}
+
+
