2eb1d2740d94ee4e7ce5561730cd0c63da095663
Pedro Girardi <pedrogirardi16@gmail.com>
2018-12-10 13:41:02 -0600

    for loops?

--- mergingAndRelabeling.R
+++ mergingAndRelabeling.R
@@ -7,3 +7,10 @@ df <- list.files(pattern = ".rds") %>%
 # try own for loop
 reduced_list = list.files(pattern = ".rds")[1:10]
 reduced_df = map_dfr(reduced_list, readRDS)
+
+
+# for loop for converting large csv files into smaller csv files with only variables of interest
+reduced_list = list.files(pattern = ".rds")[1:10]
+for (file in filelist){
+  
+}
